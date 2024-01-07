4a12fb88021642cea77d252eec6ea7ec9333fda1
ahorat <horat@lem.ee.ethz.ch>
2024-01-07 10:25:34 +0100

    Added resistors script and minor changes in capacitors script (no rerun)

--- resistors.py
+++ resistors.py
@@ -1,6 +1,6 @@
 from math import log10, floor
 import requests
-
+import re
 import os
 from pathlib import Path
 
@@ -18,19 +18,14 @@ os.environ['DIGIKEY_STORAGE_PATH'] = str(CACHE_DIR)
 API_LIMIT = {}
 
 ################## ORDER CONFIG ######################################
-BASE_ORDER_CODE = 'CC0603 NPO9BN'
-DIELECTRIC = 'C0G'
+BASE_ORDER_CODE = 'RC0603FR-.{2}'
 
-#BASE_ORDER_CODE = 'CC0603 X7R9BB'
-#DIELECTRIC = 'X7R'
-
-VOLTAGE_RATING = 50
 PACKAGE = "0603"
-#E 12
-E_LIST = [10,12,15, 18 ,22, 27, 33, 39, 47, 56, 68,82]
+#E 24
+E_LIST = [10, 11, 12, 13, 15, 16, 18 ,20, 22, 24, 27, 30, 33, 36, 39, 43, 47, 51, 56, 62, 68, 75, 82, 91]
 GAINS = [1,10,100]
-UNITS = ["P", "N", "U"]
-UNIT_GAIN = {"P":1, "N":1000, "U":1000000}
+UNITS = ["R", "K", "M"]
+UNIT_GAIN = {"R":1, "K":1000, "M":1000000}
 ################## END ORDER CONFIG ##################################
 
 
@@ -41,30 +36,34 @@ def get_order_code(productNumber):
     global API_LIMIT
     print(F"Check Product {productNumber}")
     # Generate Request with product number and maximum 10 entries. Market Place products are axcluded. 
-    search_request = KeywordSearchRequest(keywords=productNumber, record_count=10, exclude_market_place_products=True)
+    search_request = KeywordSearchRequest(keywords=productNumber.replace('.{2}',' '), record_count=20, exclude_market_place_products=True)
     # Place request on digikey webservice
     result = digikey.keyword_search(body=search_request, api_limits=API_LIMIT)
+    # unfortunate manufacturer order code with 2-4 letters for resistance value and two digits for packaging in front need an extra verification step
+    # to verify the correct resistance value. 
+    pattern = re.compile(productNumber)
     # select results if packaging is CUT-Tape (CT) and minimum order quantity is 1
-    print(result)
     orderable_parts = [(t.standard_pricing[-1].unit_price, t.manufacturer.value, t.manufacturer_part_number, t.digi_key_part_number,  t.primary_datasheet) 
-        for t in result.products if t.packaging.value_id == '2' and t.minimum_order_quantity == 1]
+        for t in result.products if t.packaging.value_id == '2' and t.minimum_order_quantity == 1 and pattern.match(t.manufacturer_part_number)]
     # Sord by price
     orderable_parts = sorted(orderable_parts, key=lambda a: a[0])
     print(F"Got Parts: {orderable_parts}") 
     return orderable_parts
 
-def get_code(Prefix, value):
+def get_code(Prefix, value, unit):
     '''
-    get Capacitor code for specific value
+    get resistance code for specific value
     '''
     value = value
     # R Notation
-    if(value <100):
-        first = floor(value/10)
-        return F"{Prefix}{first}R{floor((value-first*10))}"
-    else:        
-        base10 = floor(log10(abs(value/10)))
-        return F"{Prefix}{floor(value/pow(10,base10))}{base10-1}"
+    first = floor(value/10)
+    decimal = floor((value-first*10))
+    if(decimal != 0):
+        return F"{Prefix}{first}{unit}{decimal}"
+    else:
+        return F"{Prefix}{first}{unit}"
+    
+
 
 if __name__ == '__main__':
     with open('output.txt', 'w') as f:
@@ -72,15 +71,13 @@ if __name__ == '__main__':
             for gain in GAINS:
                 for e_value in E_LIST:
                     value =e_value*gain
-                    if(value>=100):
-                        unitvalue = F"{round(value/10)}{unit}"
-                    else:
-                        unitvalue = F"{value/10}{unit}"
-                    orderable_parts = get_order_code(get_code(BASE_ORDER_CODE, value*UNIT_GAIN[unit]))
+                    unitvalue = get_code("", value, unit)
+                    # Digikey can not search with only two letters -> L at the end is necessary so "1R" gets "1RL"
+                    orderable_parts = get_order_code(get_code(BASE_ORDER_CODE, value, unit)+"L")
                     
                     if(len(orderable_parts) > 0):
-                        f.write(F"{unitvalue}/{PACKAGE}/{VOLTAGE_RATING}V/{DIELECTRIC}	{unitvalue}/{PACKAGE}/{VOLTAGE_RATING}V/{DIELECTRIC}	CAP SM {PACKAGE} {unitvalue.lower()}F {VOLTAGE_RATING}V {DIELECTRIC} " + 
-                        F"	GENERIC	{PACKAGE}	{unitvalue.lower()}F	SCH/C_EU.SchLib	C	PCB/SMT_CHIP.PcbLib	CAP{PACKAGE}_{DIELECTRIC}	{value*UNIT_GAIN[unit]*1e-13:.2}	" + 
+                        f.write(F"{unitvalue}/{PACKAGE}	{unitvalue}/{PACKAGE}	RES SM {PACKAGE} {unitvalue} " + 
+                        F"	GENERIC	{PACKAGE}	{unitvalue}	SCH/R_IEC.SchLib	R	PCB/SMT_CHIP.PcbLib	R{PACKAGE}-0.40-WW	{value*UNIT_GAIN[unit]*1e-1:.2}	" + 
                         F"{orderable_parts[0][4]}")
                         i=0
                         for t in orderable_parts: 
